295b259a928a864987e62e6c865ed085b04d330c
qiankun <qiankun@fordeal.com>
2021-12-25 16:18:19 +0800

    第一道hard题

---
+++ src/com/me/string/SubarraysWithKDistinct.java
@@ -0,0 +1,53 @@
+package com.me.string;
+
+/**
+ * hard题。需要先理解最多k个不同整数构成的字符串个数的解法。
+ *
+ * 给定一个正整数数组 A，如果 A 的某个子数组中不同整数的个数恰好为 K，则称 A 的这个连续、不一定不同的子数组为好子数组。
+ * <p>
+ * （例如，[1,2,3,1,2] 中有 3 个不同的整数：1，2，以及 3。）
+ * <p>
+ * 返回 A 中好子数组的数目。
+ *
+ * @author qiankun
+ * @version 2021/12/25
+ */
+public class SubarraysWithKDistinct {
+    public static int subarraysWithKDistinct(int[] A, int K) {
+        return atMostKDistinct(A, K) - atMostKDistinct(A, K - 1);
+    }
+
+    private static int atMostKDistinct(int[] A, int K) {
+        int len = A.length;
+        int[] freq = new int[len + 1];
+
+        int left = 0;
+        int right = 0;
+        // [left, right) 里不同整数的个数
+        int count = 0;
+        int res = 0;
+        // [left, right) 包含不同整数的个数小于等于 K
+        while (right < len) {
+            if (freq[A[right]] == 0) {
+                count++;
+            }
+            freq[A[right]]++;
+            right++;
+
+            while (count > K) {
+                freq[A[left]]--;
+                if (freq[A[left]] == 0) {
+                    count--;
+                }
+                left++;
+            }
+            // [left, right) 区间的长度就是对结果的贡献
+            res += right - left;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(subarraysWithKDistinct(new int[]{1, 2, 1, 2, 3}, 2));
+    }
+}
