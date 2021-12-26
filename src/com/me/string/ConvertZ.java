e5b9953512a25423c3ab7683f664999e6a90137c
qiankun <qiankun@fordeal.com>
2021-12-27 00:00:17 +0800

    add

--- src/com/me/string/ConvertZ.java
+++ src/com/me/string/ConvertZ.java
@@ -4,6 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ *
+ * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ *
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zigzag-conversion
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * @author qiankun
  * @version 2021/12/26
  */
