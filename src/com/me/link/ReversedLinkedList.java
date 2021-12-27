740415202f25b3a868914399d094adb5d3ae8482
qiankun <qiankun@fordeal.com>
2021-12-27 23:47:19 +0800

    注释

--- src/com/me/link/ReversedLinkedList.java
+++ src/com/me/link/ReversedLinkedList.java
@@ -3,6 +3,16 @@ package com.me.link;
 import java.util.List;
 
 /**
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ *  
+ * 示例 1：
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * @author qiankun
  * @version 2021/12/27
  */
