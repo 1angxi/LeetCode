4fe15d7173cc7c4c094516d4f5cbf98f00a2ff3b
qiankun <qiankun@qiankundeMacBook-Pro.local>
2022-01-02 10:24:09 +0800

    fix

---
+++ src/com/me/backtrace/GrayCode.java
@@ -0,0 +1,59 @@
+package com.me.backtrace;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * n 位格雷码序列 是一个由 2n 个整数组成的序列，其中：
+ * 每个整数都在范围 [0, 2n - 1] 内（含 0 和 2n - 1）
+ * 第一个整数是 0
+ * 一个整数在序列中出现 不超过一次
+ * 每对 相邻 整数的二进制表示 恰好一位不同 ，且
+ * 第一个 和 最后一个 整数的二进制表示 恰好一位不同
+ * 给你一个整数 n ，返回任一有效的 n 位格雷码序列 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/gray-code
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class GrayCode {
+
+    /**
+     * 镜像法；res[n] 等于 res[n-1] + res[n-1]的反向前面加1；刚好符合每次只变化1位
+     */
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        res.add(0);
+
+        int head = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = res.size() - 1; j >= 0; j--) {
+                res.add(head + res.get(j));
+            }
+            head = head << 1;
+        }
+
+        return res;
+    }
+
+
+    public List<Integer> grayCodeV2(int n) {
+        /*
+         关键是搞清楚格雷编码的生成过程, G(i) = i ^ (i/2);
+         如 n = 3:
+         G(0) = 000,
+         G(1) = 1 ^ 0 = 001 ^ 000 = 001
+         G(2) = 2 ^ 1 = 010 ^ 001 = 011
+         G(3) = 3 ^ 1 = 011 ^ 001 = 010
+         G(4) = 4 ^ 2 = 100 ^ 010 = 110
+         G(5) = 5 ^ 2 = 101 ^ 010 = 111
+         G(6) = 6 ^ 3 = 110 ^ 011 = 101
+         G(7) = 7 ^ 3 = 111 ^ 011 = 100
+         **/
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < 1 << n; ++i)
+            ret.add(i ^ i >> 1);
+        return ret;
+    }
+
+}
