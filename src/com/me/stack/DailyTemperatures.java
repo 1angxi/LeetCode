304e152b3109cab2d87bd7db3fd240e3bed3d5ab
qiankun <qiankun@qiankundeMacBook-Pro.local>
2022-01-03 16:15:52 +0800

    fix

---
+++ src/com/me/stack/DailyTemperatures.java
@@ -0,0 +1,44 @@
+package com.me.stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 请根据每日 气温 列表 temperatures ，请计算在每一天需要等几天才会有更高的温度。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+ *
+ * 示例 1:
+ *
+ * 输入: temperatures = [73,74,75,71,69,72,76,73]
+ * 输出: [1,1,4,2,1,1,0,0]
+ * 示例 2:
+ *
+ * 输入: temperatures = [30,40,50,60]
+ * 输出: [1,1,1,0]
+ * 示例 3:
+ *
+ * 输入: temperatures = [30,60,90]
+ * 输出: [1,1,0]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/daily-temperatures
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class DailyTemperatures {
+    /**
+     * 通过单调栈存储小温度的下标。当碰到高温的时候，直接下标相减就能得到小温度到大温度的天数差距；
+     */
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> stack = new LinkedList<>();
+        int[] res = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int index = stack.pop();
+                res[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        return res;
+
+    }
+}
