df60204157d88a8aa6bc20f1ee893b16988017ce
xy300007 <xy300007@163.com>
2022-01-06 01:08:01 +0800

    二叉树种类

---
+++ src/com/me/dynamic/MaximalSquare.java
@@ -0,0 +1,44 @@
+package com.me.dynamic;
+
+/**
+ * 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * 输出：4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximal-square
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MaximalSquare {
+    public int maximalSquare(char[][] matrix) {
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] dp = new int[matrix.length][matrix[0].length];
+
+        /**
+         * 诀窍是用动态规划；找到状态转移函数比较困难，需要记忆下来；
+         */
+        int max = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i - 1][j - 1]), dp[i][j - 1]) + 1;
+                    }
+                    max = Math.max(dp[i][j], max);
+                }
+            }
+        }
+
+        return max * max;
+    }
+}
